c4003c58ee392e990f83ffb59ccc1840738741cf
Felipe Santana Maffezzolli <felipemaffezzolli@Mac-mini-de-Claudio.local>
2020-03-27 15:54:17 -0300

    pagination, cors

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -2,7 +2,24 @@ const connection = require('../database/connection');
 
 module.exports = {
   async index (request, response) {
-    const incidents = await connection('incidents').select('*');
+    const { page = 1 } = request.query;
+
+    const [count] = await connection('incidents').count();
+
+    response.header('X-Total-Count', count['count(*)'])
+
+    const incidents = await connection('incidents')
+      .join('ongs', 'ong_id', '=', 'incidents.ong_id')
+      .limit(5)
+      .offset((page - 1) * 5)
+      .select([
+        'incidents.*',
+        'ongs.name',
+        'ongs.email',
+        'ongs.whatsapp',
+        'ongs.city',
+        'ongs.uf'
+      ]);
 
     return response.json(incidents)
   },
