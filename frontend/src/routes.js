6f770878219f560b3dde28290fd5265ee438e2d7
Felipe Maffezzolli <felipe@cm2tech.com.br>
2020-03-27 20:44:43 -0300

    finished frontend styling

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 // pages
 import Logon from './pages/Logon'
 import Register from './pages/Register'
+import Profile from './pages/Profile'
+import NewIncident from './pages/NewIncident'
 
 export default function src() {
   return (
@@ -11,6 +13,8 @@ export default function src() {
       <Switch>
         <Route path="/" exact component={Logon} />
         <Route path="/register" component={Register} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/incidents/new" component={NewIncident} />
       </Switch>
     </BrowserRouter>
   );
