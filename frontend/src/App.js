63cb05bfad79d94ca68d074ddc3f784aacc32100
Felipe Maffezzolli <felipe@cm2tech.com.br>
2020-03-27 16:47:47 -0300

    add front end

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Logon from './pages/Logon'
+
+function App() {
+  return (
+    <Logon />
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+export default function Header(props) {
+  return (
+    <header>
+      <h1>{ props.title }</h1>
+    </header>
+  );
+}
